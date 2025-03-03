f74ed2d4970106959ffb672aae1c4db568f5c6fe
elitis91 <Elitisconsulting@proton.me>
2025-03-03 19:17:09 +0100

    HATEOAS implementation
    Implémentation HATEOAS dans une API Spring Boot pour la ressource.

--- atelier_simple_rest_api/src/main/java/com/formationorsys/atelier/controllers/BookController.java
+++ atelier_simple_rest_api/src/main/java/com/formationorsys/atelier/controllers/BookController.java
@@ -2,8 +2,11 @@ package com.formationorsys.atelier.controllers;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -26,54 +29,74 @@ public class BookController {
 	@Autowired
 	BookService bookService;
 	
-	// Récupérer l'ensemble des livres
+	// Récupération de l'ensemble des livres
 	@GetMapping(path = "/books", produces = "application/json")
-	public ResponseEntity<List<Book>> getBooks(){
-		try {
-			return new ResponseEntity<List<Book>>(bookService.getBooks(),HttpStatus.OK);
-		} catch (Exception e) {
-			return new ResponseEntity<List<Book>>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
+	public ResponseEntity<List<EntityModel<Book>>> getBooks(){
+		List<Book> books = bookService.getBooks();
+		
+		List<EntityModel<Book>> bookRessources = books.stream().map(book -> {
+			EntityModel<Book> resource = EntityModel.of(book);
+			resource.add(WebMvcLinkBuilder.linkTo(
+					WebMvcLinkBuilder.methodOn(BookController.class).getBookByID(book.getId())
+					).withRel("all-books"));
+			
+			return resource;
+		}).collect(Collectors.toList());
+		
+		return ResponseEntity.ok(bookRessources);	
+	}	
 	
 	// Récupérer un livre au moyen de son ID
 	@GetMapping(path = "/books/{id}", produces = "application/json")
-	public ResponseEntity<Book> getBookByID(@PathVariable Long id){
-		try {
-			return new ResponseEntity<Book>(
-					bookService.getBookByID(id),
-					HttpStatus.OK
-					);
-		} catch (Exception e) {
-			// TODO: handle exception
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
+	public ResponseEntity<EntityModel<Book>> getBookByID(@PathVariable Long id){
+		
+		Book book = bookService.getBookByID(id);
+        EntityModel<Book> resource = EntityModel.of(book);
+        resource.add(WebMvcLinkBuilder.linkTo(
+            WebMvcLinkBuilder.methodOn(BookController.class).getBookByID(id)
+        ).withSelfRel());
+        
+        resource.add(WebMvcLinkBuilder.linkTo(
+                WebMvcLinkBuilder.methodOn(BookController.class).getBooks()
+            ).withRel("all-books"));
+
+        return ResponseEntity.ok(resource);	
 	}
+	
 	
 	// Ajouter un livre
 	@PostMapping(path = "/books", produces = "application/json")
-	public ResponseEntity<Book> addBook(@RequestBody Book book){
-		try {
-			return new ResponseEntity<Book>(
-					bookService.addBooks(book),
-					HttpStatus.CREATED
-					);		
-		} catch (Exception e) {
-			// TODO: handle exception
-			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+	public ResponseEntity<EntityModel<Book>> addBook(@RequestBody Book book){
+		
+        Book savedBook = bookService.addBooks(book);
+
+        EntityModel<Book> resource = EntityModel.of(savedBook);
+        resource.add(WebMvcLinkBuilder.linkTo(
+            WebMvcLinkBuilder.methodOn(BookController.class).getBookByID(savedBook.getId())
+        ).withSelfRel());
+
+        return ResponseEntity.created(
+            WebMvcLinkBuilder.linkTo(
+                WebMvcLinkBuilder.methodOn(BookController.class).getBookByID(savedBook.getId())
+            ).toUri()
+        ).body(resource); 
+
 	}
 	
 	// Mise à jour un livre
 	@PutMapping(path = "/books/{id}", produces = "application/json")
-	public ResponseEntity<Book> updateBook(@RequestBody Book book, @PathVariable Long id){
-		try {
-			return new ResponseEntity<Book>(bookService.updateBook(book, id), HttpStatus.OK);
-		} catch (Exception e) {
-			// TODO: handle exception
-			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+	public ResponseEntity<EntityModel<Book>> updateBook(@RequestBody Book book, @PathVariable Long id){
+		
+        Book updatedBook = bookService.updateBook(book, id);
+
+	    EntityModel<Book> resource = EntityModel.of(updatedBook);
+	    resource.add(WebMvcLinkBuilder.linkTo(
+	        WebMvcLinkBuilder.methodOn(BookController.class).getBookByID(id)
+	    ).withSelfRel());
+	
+	    return ResponseEntity.ok(resource);
 	}
+	
 	
 	@DeleteMapping(path = "/books/{id}")
 	public ResponseEntity<HttpStatus> deleteBookByID(@PathVariable Long id){
